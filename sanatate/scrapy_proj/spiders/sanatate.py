eea774716d0ce2d84e93c520bee33faac307c2d9
Ubuntu <owlree128@gmail.com>
2017-03-11 13:28:34 +0000

    added pagination

--- sanatate/scrapy_proj/spiders/sanatate.py
+++ sanatate/scrapy_proj/spiders/sanatate.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+# temporary remove whitespace function
+def rws(str):
+    if str:
+        return ' '.join(str.split())
+    else:
+        return None
 
 class SanatateSpider(scrapy.Spider):
     name = "sanatate"
@@ -16,5 +22,10 @@ class SanatateSpider(scrapy.Spider):
     def parse(self, response):
 
         for title in response.css('.panel-group div.panel-heading a::text').extract():
-            # print(title)
-            yield {'title': title}
+            yield {'title': rws(title)}
+
+        next_pages = response.css('.pt-cv-pagination a::attr(href)').extract()
+        next_pages.reverse()
+        for next_page in next_pages:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
