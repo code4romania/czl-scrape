6aca1d01e64f7900d3905813a9db6397f78cb024
Rares Urdea <rares@imedicare.com>
2017-03-11 12:03:54 +0200

    initial commit

---
+++ externe/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import re
+
+from setuptools import setup
+
+install_requires = [
+    'beautifulsoup4',
+    'requests',
+    'simplejson'
+]
+
+version_regex = re.compile("VERSION\s*=\s*'(.*?)'$")
+
+with open('core/__init__.py') as stream:
+    VERSION = version_regex.search(stream.read()).group(1)
+
+setup(
+    version=VERSION,
+    name='mae-scraper',
+    url='https://github.com/code4romania/czl-scrape/tree/master/externe',
+    author='',
+    author_email='',
+    description='Scraper pentru site-ul Ministerului de afaceri externe',
+    install_requires=install_requires,
+)
+\
