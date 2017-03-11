3cad9b5694f59981f98ae46f6c6652e7a549017b
Aeon Glass <aeonglass@localhost.localdomain>
2017-03-11 19:56:36 +0000

    Added the script for scrapping MAPN

---
+++ afaceri/aippimm_plugin.php
@@ -0,0 +1,123 @@
+<?php
+
+$strMainURL = 'http://www.aippimm.ro/categorie/propuneri_lg/';
+$strSubMainURL = 'http://www.aippimm.ro/categorie/transparenta-decizionala---modificare-hg-96-2011/';
+$strPostURl = 'http://czl-api.code4.ro/api/publications/';
+$arrPostDocuments = array();
+
+$strContent= _getURL($strSubMainURL);
+if($strContent)
+{
+    if(preg_match_all('%<a class="lead_subcat" href="(?<url_article>.*?)" title="(?<title>.*?)"><strong>.*?<\/strong><\/a> - (?<date>\d{1,2}\.\d{1,2}\.\d{1,4}).*?<div class="files_container">(?<docs_raw>.*?)<\/div>%s', $strContent, $arrArticlesInfo))
+    {
+        $intCount = count($arrArticlesInfo[0]);
+        if($intCount !== 0)
+        {
+            for($intI = 0; $intI < $intCount; $intI++)
+            {
+                $arrItem = array(
+                    'identifier' => '',
+                    'title' => '',
+                    'type' => '',
+                    'institution' => 'afaceri',
+                    'date' => '',
+                    'description' => '',
+                    'feedback_days' => '',
+                    'contact' => array('email' => 'directia.imm@imm.gov.ro'),
+                    'documents' => array()
+                );
+                $arrItem['title'] = $arrArticlesInfo['title'][$intI];
+                $arrItem['description'] = $arrItem['title'];
+                $arrItem['identifier'] = str_replace(' ', '-', strtolower(substr($arrItem['title'], 0, 40))) . time();
+                $date = DateTime::createFromFormat('d.m.Y', $arrArticlesInfo['date'][$intI]);
+                $output = $date->format('Y-m-d');
+                $arrItem['date'] = $output;
+                $type = _getCorrespondingType($arrItem['title']);
+                $arrItem['type'] = ($type) ? $type : '';
+
+                if(preg_match_all('%href="(.*?)"%s', $arrArticlesInfo['docs_raw'][$intI], $arrDocuments))
+                {
+                    foreach($arrDocuments[1] as $key=>$document)
+                    {
+                        $arrItem['documents'][$key]['url'] = $document;
+                        $arrItem['documents'][$key]['type'] = 'oug';
+                    }
+                    array_push($arrPostDocuments, $arrItem);
+                    unset($arrItem);
+                }
+                else
+                {
+                    print_r('!!NOTICE!! Articolul nu are documente' . PHP_EOL);
+                }
+            }
+            print_r($arrPostDocuments);
+            //api call
+            foreach($arrPostDocuments as $document)
+            {
+                $jsonEncoded = json_encode($document);
+                //API CALL
+//                $strResponse = _getURL($strPostURl, $jsonEncoded);
+//                print_r($strResponse);
+            }
+            return true;
+        }
+    }
+    else
+    {
+        print_r('!!ERROR!! Informatiile pentru articole nu au putut fi luate' . PHP_EOL);
+        return false;
+    }
+}
+else
+{
+    print_r('!!ERROR!! Pagina de transparenta decizionala nu este functionala sau a fost schimbata, verifica ' . $strMainURL . PHP_EOL);
+    return false;
+}
+
+return false;
+
+
+function _getCorrespondingType($strTitle){
+    if(strpos($strTitle, 'OUG') !== false || strpos($strTitle, 'Ordonanta de Urgenta') !== false)
+        return 'OUG';
+    else if(strpos($strTitle, 'HG') !== false)
+        return 'HG';
+    else
+        return 'OTHER';
+}
+
+
+function _getURL($strURL, $mxdPost = false){
+    $intRetries = 5;
+    $strResponse = false;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$strURL);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
+    curl_setopt($ch,CURLOPT_TIMEOUT, 20);
+    $header = array('User-Agent: Jesus');
+    curl_setopt($ch,CURLOPT_HTTPHEADER, $header);
+    if($mxdPost !== false)
+    {
+        array_push($header, "Content-type: application/json");
+        array_push($header, "Authorization: afaceri-very-secret-key");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $mxdPost);
+
+    }
+    for($intI=0; $intI<$intRetries; $intI++)
+    {
+        $strResponse = curl_exec($ch);
+        $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if($responseCode >= 300 || $strResponse == '')
+            continue;
+        break;
+    }
+    if(!$strResponse)
+        return false;
+    curl_close($ch);
+    return $strResponse;
+}
+
+?>
---
+++ aparare/mapn_plugin.php
@@ -0,0 +1,167 @@
+<?php
+
+$strMainURL = 'http://dlaj.mapn.ro/';
+//$strMainURL = 'http://dlaj.mapn.ro/arhiva2016.php';
+$strPostURl = 'http://czl-api.code4.ro/api/publications/';
+
+$strContent = _getURL($strMainURL); //for prod
+//$strContent = file_get_contents('mapn.txt'); //for debug
+$strContent = html_entity_decode(mb_convert_encoding($strContent, 'UTF-8', 'ISO-8859-2'));
+
+$arrPostDocuments = array();
+if($strContent)
+{
+    if(preg_match_all('%<img src="assets\/premium\.gif" width="\d{1,}" height="\d{1,}" \/>(.*?)<\/strong>%s', $strContent, $arrTitles))
+    {
+        if(preg_match_all('%<em>FISIERE DISPONIBILE(.*?)<\/em>%s', $strContent, $arrAvailableDocuments))
+        {
+            if(preg_match_all('%<em> Propunerile,.*?adresa de e-mail(.*?),.*?termen de ([0-9]+) de%s', $strContent, $arrProposals))
+            {
+
+                if (count($arrAvailableDocuments[0]) !== count($arrTitles[0])) {
+                    print_r('!!ERROR!! Numar diferit de taguri de documente si titluri de documente (proiect fara documente)' . PHP_EOL);
+                    return false;
+                }
+                foreach ($arrAvailableDocuments[1] as $keyRaw => $arrAvailableDocumentsRaw) {
+                    $arrItem = array(
+                        'identifier' => '',
+                        'title' => '',
+                        'type' => '',
+                        'institution' => 'aparare',
+                        'date' => '',
+                        'description' => '',
+                        'feedback_days' => '',
+                        'contact' => array(),
+                        'documents' => array()
+                    );
+                    $strTitle = str_replace("\n", '', str_replace("\r", ' ', trim($arrTitles[1][$keyRaw])));
+                    while (strpos($strTitle, '  ') !== false)
+                        $strTitle = str_replace("  ", ' ', $strTitle);
+                    $arrItem['title'] = $strTitle;
+                    $arrItem['description'] = $strTitle;
+                    $strType = _getCorrespondingType($arrItem['title']);
+                    $arrItem['type'] = ($strType) ? $strType : '';
+                    $arrItem['contact']['email'] = trim($arrProposals[1][$keyRaw],chr(0xC2).chr(0xA0));
+                    $arrItem['feedback_days'] = intval($arrProposals[2][$keyRaw]);;
+                    if (preg_match_all('%<a href="(http:\/\/dlaj\.mapn\.ro\/documents\/acts\/(.*?)\..*?)">%s', $arrAvailableDocumentsRaw, $arrDocuments)) {
+                        foreach ($arrDocuments[1] as $key => $strDocumentLink) {
+                            $arrItem['documents'][$key]['url'] = $strDocumentLink;
+                            $arrItem['documents'][$key]['type'] = 'proiect_act_normativ';
+                        }
+                        $arrItem['identifier'] = str_replace('_', '-', strtolower($arrDocuments[2][0]));
+                    } else {
+                        print_r('!!NOTICE!! Nu am gasit documente pentru ' . $arrTitles[1][$keyRaw] . PHP_EOL);
+                    }
+                    if (preg_match('%\(DATA AFISARII: (.*?)\)%s', $arrAvailableDocumentsRaw, $arrDate)) {
+                        $arrItem['date'] = $arrDate[1];
+                    } else {
+                        print_r('!!NOTICE!! Nu am gasit documente pentru ' . $arrTitles[1][$keyRaw] . PHP_EOL);
+                    }
+                    if (count($arrItem) > 0) {
+                        array_push($arrPostDocuments, $arrItem);
+                    }
+                    unset($arrItem);
+                }
+            }
+            else
+            {
+                print_r('!!ERROR!! Datele de feedback nu au putut fi luate');
+                return false;
+            }
+        }
+        else
+        {
+            print_r('!!ERROR!! Documentele nu au putut fi luate');
+            return false;
+        }
+        print_r($arrPostDocuments);
+        foreach($arrPostDocuments as $document)
+        {
+            $jsonEncoded = json_encode($document);
+            //API CALL
+            $strResponse = _getURL($strPostURl, $jsonEncoded);
+            print_r($strResponse);
+        }
+        return true;
+    }
+    else
+    {
+        print_r('!!ERROR!! Informatiile nu au putut fi luate');
+        return false;
+    }
+}
+else
+{
+    print_r("!!ERROR!! Siteul a picat. Incearca mai tarziu \n");
+    print_r($strContent);
+}
+return false;
+
+function _getCorrespondingType($strTitle){
+    $arrWords = explode(' ',$strTitle);
+    $strFirstWordsOfTitle = trim(strtolower(current($arrWords)), chr(0xC2).chr(0xA0));
+    switch($strFirstWordsOfTitle)
+    {
+        case 'hotărâre':
+        case 'hotărârea':
+            return 'HG';
+        case 'proiectul':
+        case 'proiect':
+            if(strpos($strTitle, 'hotărâre') !== false)
+                return 'HG';
+            else if(strpos($strTitle, 'ordin') !== false)
+                return 'OM';
+            else
+                return 'OTHER';
+        case 'ordin':
+            return 'OM';
+        case 'ordonanţă':
+            if(strpos($strTitle, 'urgenţă') !== false)
+                return 'OUG';
+            else
+                return 'OG';
+        case 'lege':
+            return 'LEGE';
+        case 'dezbatere':
+        case 'anuntprivind':
+            return 'OTHER';
+        default:
+            print_r("!!NOTICE!! Tip nou de element: " . $strFirstWordsOfTitle . PHP_EOL);
+            return 'OTHER';
+    }
+}
+
+function _getURL($strURL, $mxdPost = false){
+    $intRetries = 5;
+    $strResponse = false;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$strURL);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
+    curl_setopt($ch,CURLOPT_TIMEOUT, 20);
+    $header = array('User-Agent: Jesus');
+    curl_setopt($ch,CURLOPT_HTTPHEADER, $header);
+    if($mxdPost !== false)
+    {
+        array_push($header, "Content-type: application/json");
+        array_push($header, "Authorization: aparare-very-secret-key");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_POST, true);c
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $mxdPost);
+
+    }
+    for($intI=0; $intI<$intRetries; $intI++)
+    {
+        $strResponse = curl_exec($ch);
+        $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if($responseCode >= 300 || $strResponse == '')
+            continue;
+        break;
+    }
+    if(!$strResponse)
+        return false;
+    curl_close($ch);
+    return $strResponse;
+}
+
+?>
