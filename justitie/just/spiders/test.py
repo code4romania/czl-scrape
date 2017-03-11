a94fbf233f9fd56272a5748dd53ecbe9efa7f492
Vlad Fratila <vlad.fratila@gmail.com>
2017-03-11 16:44:40 +0200

    add parsing for date and identifier

--- justitie/just/spiders/test.py
+++ justitie/just/spiders/test.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
+
+import datetime
+import locale
+locale.setlocale(locale.LC_ALL, "ro_RO.UTF-8")
+
+from unidecode import unidecode
+import re
+
 from scrapy.loader import ItemLoader
 
 from items import JustPublication
@@ -14,7 +22,23 @@ class TestSpider(scrapy.Spider):
 
     def parse(self, response):
         for li_item in response.css('#content div.entry-content ul.lcp_catlist li'):
-            title = li_item.css('h3.lcp_post a::text').extract_first()
-            item = JustPublication(title=title)
+            title = li_item.css('h3.lcp_post a::text').extract_first().strip()
+            text_date = li_item.css('::text').extract_first().strip()
+
+            date = datetime.datetime.strptime(text_date, '%d %B %Y')
+            date = date.date().isoformat()
+
+            item = JustPublication(
+                title=title,
+                identifier=self.slugify("%s-%s" % (text_date, title) ),
+                date=date
+            )
+
+            self.logger.info(item.items())
+
             yield item
         pass
+
+    def slugify(self, text):
+        text = unidecode(text).lower()
+        return re.sub(r'\W+', '-', text)
