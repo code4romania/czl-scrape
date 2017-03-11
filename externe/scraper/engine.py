c6143c0d5b06bc35d790ba265507324499ea6478
Rares Urdea <rares@imedicare.com>
2017-03-11 16:19:32 +0200

    work in progress

--- externe/scraper/engine.py
+++ externe/scraper/engine.py
@@ -26,9 +26,9 @@ class Extractor:
     tables = self.content.select_one('div.art').select('table')
     for table in tables:
       article = Article(table)
-      print('\n title: %s \n description: %s \n urls: %s \n published: %s \n'
+      print('\n title: %s \n description: %s \n urls: %s \n published: %s \n debate_until: %s \n'
             % (article.article_type, article.description,
-               article.documents, article.published_at))
+               article.documents, article.published_at, article.debate_until))
       yield article
 
 
@@ -36,12 +36,13 @@ class Article:
   """
   Defines an article object as found on the MAE site.
   """
-  DATE_REGX = '([0-9]{2})(.*?)([0-9]{4})'
+  DATE_REGX = '([0-9]+)(.*?)([0-9]{4})'
+  TIMEDELTA_REGX = '(timp\sde\s([0-9]{2})\szile)'
   DESCRIPTION_FMT = '{0} {1}'
 
   def __init__(self, table):
     """
-    Builds an Article object from a given HTML table.
+    Builds an Article object from a given HTML table row
     :param table: the table.
     :return: the current object.
     """
@@ -52,6 +53,7 @@ class Article:
       self.article_type = self._extract_article_type(tr)
       self._extract_description(tr)
       self._extract_published_at(tr)
+      self._extract_debate_until(tr)
     except Exception:
       print('Unable to build article from table')
 
@@ -59,6 +61,7 @@ class Article:
   description = None
   documents = None
   published_at = None
+  debate_until = None
   contact = None
   institution = settings.INSTITUTION
 
@@ -89,7 +92,7 @@ class Article:
 
   def _extract_article_type(self, row):
     """
-    extracts the title from a given HTML table.
+    extracts and sets the title from a given HTML table row
     :param row: the given table row
     :return: String
     """
@@ -97,30 +100,45 @@ class Article:
 
   def _extract_description(self, row):
     """
-    extracts the description from a given HTML table.
+    extracts and sets the description from a given HTML table row
     :param table: the given table row
     :return: None
     """
     art_type = self._extract_article_type(row).lower().capitalize()
-    desc_text = row[0].find_all('strong')[1].text
+    desc_text = row[0].find_all('strong')[1].text.rstrip('\n')
     self.description = self.DESCRIPTION_FMT.format(art_type, desc_text)
 
   def _extract_published_at(self, row):
     """
-    extracts the published_at attribute from a given HTML table.
+    extracts and sets the published_at attribute from a given HTML table row.
     :param row: the given table row
     :return: None
     """
-    desc_paragraph = self._extract_desc_paragraph(row)
-    match = re.search(self.DATE_REGX, desc_paragraph)
-    month = settings.MONTHS.get(match.group(2).strip())
-    if not month:
-      print('Unable to match month for date: %s' % match.group(0))
+    published_text = row[-1].find_all('p')[-1].text
+    match = re.search(self.DATE_REGX, published_text)
+    if match:
+      self.published_at = self._build_date_from_match(match, d=1, m=2, y=3)
+
+  def _extract_debate_until(self, row):
+    """
+    extracts and sets the debate_until attribute from a given HTML table row.
+    :param row: the given table row
+    :return: None
+    """
+    desc_text = row[-1].find_all('p').text
+    match = re.search(self.DATE_REGX, desc_text)
+    if match:
+      self.debate_until = self._build_date_from_match(match, d=2, m=3, y=4)
+      # In case no direct date is provided, try timedelta.
     else:
-      self.published_at = date(
-        int(match.group(3)), int(month), int(match.group(1))
+      delta_match = re.search(self.TIMEDELTA_REGX, desc_text)
+      delta = delta_match.group(1)
+
+  def _build_date_from_match(self, match, d, m, y):
+    month = settings.MONTHS.get(match.group(m).strip())
+    if not month:
+      print('Unable to match month for date string: %s' % match.group(0))
+    else:
+      return date(
+        year=int(match.group(y)), month=int(month), day=int(match.group(d))
       )
-
-
-  def _extract_desc_paragraph(self, row):
-    return row[2].select('td')[0].select('p')[1].text
