698cd326c125b67a1eb73246cf21eea44d142c25
Alexandru Hodorogea| <alexhod10@gmail.com>
2017-03-11 19:33:01 +0200

    serializer fix

--- externe/scraper/serializer.py
+++ externe/scraper/serializer.py
@@ -11,10 +11,11 @@ class Serializer:
       type=article.article_type,
       institution=settings.INSTITUTION,
       date=article.published_at.isoformat(),
-      description='',
+      description='no desc',
       feedback_days=article.feedback_days,
       contact=article.contact,
       documents=article.documents,
+      issuer=settings.INSTITUTION,
     )
     return _ret
     # return json.dumps(_ret)
