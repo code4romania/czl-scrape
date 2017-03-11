cde464932f80185a8805abd9c8bf778037251468
Vlad Fratila <vlad.fratila@gmail.com>
2017-03-11 16:04:22 +0200

    add PublicationItem

--- justitie/just/items.py
+++ justitie/just/items.py
@@ -7,8 +7,18 @@
 
 import scrapy
 
-
-class JustItem(scrapy.Item):
+class JustPublication(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    identifier = scrapy.Field()
+    title = scrapy.Field()
+    type = scrapy.Field()
+    insitution = scrapy.Field()
+    date = scrapy.Field()
+    description = scrapy.Field()
+    feedback_days = scrapy.Field()
+    contact = scrapy.Field()
+    documents = scrapy.Field()
+
     pass
+
