9e650600020601424ae1b43cf94f0243016caf26
kinko_2007 <kinko_2007@users.noreply.github.com>
2017-03-12 00:53:37 +0200

    Cleaning & posting

--- economie/parseProject.js
+++ economie/parseProject.js
@@ -1,175 +1,205 @@
-let cheerio = require('cheerio'),
-    objectHash = require('object-hash');
+/** ====== MAIN ====== */
 
-module.exports = function parseFunction($, URL) {
-  "use strict";
+function parseProject($, URL) {
+    let parsedResult = {
+        identifier: "***lawproposal-first-document-name-slug-or-something", // un identificator unic, predictibil (repetabil), pereferabil human-readable
+        title: "***Proiectul de ordin al ministrului justiției pentru aprobarea Regulamentului privind organizarea și desfășurarea activităților și programelor educative, de asistență psihologică și asistență socială din locurile de deținere aflate în subordinea Aministrației Naționale a Penitenciarelor", // titlul actului legislativ propus
+        type: "***HG", // HG, OG, OUG, PROIECT
+        institution: "economie", // ID-ul platformei din care provine actul legislativ
+        date: "***2017-03-08", // ISO 8601
+        description: null,
+        feedback_days: 0x10, // numarul zilelor disponibile pentru feedback
+        contact: {tel: null, email: "***feedback@example.org"}, // dictionar cu datale de contact. chei sugerate: "tel", "email", "addr"
+        documents: [ // array de dictionare
+            {
+                type: "***anexa", // free text momentan
+                url: "***http://www.just.ro/wp-content/uploads/2017/02/Proiect.docx" // da, este un link catre un document oficial de la MJ
+            }
+        ]
+    };
 
-  let parsedResult = {
-    identifier: "***lawproposal-first-document-name-slug-or-something", // un identificator unic, predictibil (repetabil), pereferabil human-readable
-    title: "***Proiectul de ordin al ministrului justiției pentru aprobarea Regulamentului privind organizarea și desfășurarea activităților și programelor educative, de asistență psihologică și asistență socială din locurile de deținere aflate în subordinea Aministrației Naționale a Penitenciarelor", // titlul actului legislativ propus
-    type: "***HG", // HG, OG, OUG, PROIECT
-    institution: "economie", // ID-ul platformei din care provine actul legislativ
-    date: "***2017-03-08", // ISO 8601
-    description: null,
-    feedback_days: 0x10, // numarul zilelor disponibile pentru feedback
-    contact: {tel: null, email: "***feedback@example.org"}, // dictionar cu datale de contact. chei sugerate: "tel", "email", "addr"
-    documents: [ // array de dictionare
-      {
-        type: "***anexa", // free text momentan
-        url: "***http://www.just.ro/wp-content/uploads/2017/02/Proiect.docx" // da, este un link catre un document oficial de la MJ
-      }
-    ]
-  };
+    let proposalsSuggestionsOpinionsListItem = $('ul>li').last();
+    parsedResult.identifier = getIdentifier($);
+    parsedResult.title = getTitle($);
+    parsedResult.type = getType($, parsedResult.title);
+    parsedResult.date = getDate($);
+    parsedResult.feedback_days = getFeedbackDays($, proposalsSuggestionsOpinionsListItem);
+    parsedResult.contact.email = getEmail($, proposalsSuggestionsOpinionsListItem);
+    parsedResult.documents = getDocuments($, URL);
 
-  parsedResult.identifier = getTitle($).toLowerCase().split(/\s+/).join('-').replace(/[`~!@#$%^&*()_|+=?;:'",.<>\{\}\[\]\\\/]/gi, '');
-  parsedResult.title = getTitle($);
-  parsedResult.type = getType($, parsedResult.title);
-  parsedResult.date = getDate($);
-  let proposalsSuggestionsOpinionsListItem = $('ul>li').last();
-  parsedResult.feedback_days = getFeedbackDays($, proposalsSuggestionsOpinionsListItem);
-  parsedResult.contact.email = getEmail($, proposalsSuggestionsOpinionsListItem);
-  parsedResult.documents = getDocuments($, URL);
+    console.log(JSON.stringify(parsedResult, null, 4));
 
-  console.log(JSON.stringify(parsedResult, null, 4));
-};
+    return parsedResult;
+}
+
+
+/** ====== identifier ====== */
 
 function getIdentifier($) {
-  "use strict";
-
-  return objectHash($);
+    return getTitle($)
+        .toLowerCase()
+        .replace(/\s+/, '-')
+        .replace(/[`~!@#$%^&*()_|+=?;:'",.<>\{\}\[\]\\\/]/gi, '');
 }
+
+
+/** ====== title ====== */
 
 function getTitle($) {
-  "use strict";
-
-  return $('.item.column-1>b').first().text().trim();
+    return $('.item.column-1>b')
+        .first()
+        .text()
+        .trim();
 }
+
+
+/** ====== type ====== */
 
 let regexClassificators = {
-  HG: [
-    'Hotărâre a Guvernului',
-    'Hotarare a Guvernului',
-    'HG',
-    'Hotărâre pentru',
-    'Hotarare privind',
-    'HOTĂRÂRE privind',
-    'HOTĂRÂRE pentru',
-    'HOTARARE pentru',
-    'Hotărâre de Guvern'
-  ],
-  OG: [
-    'Ordin al',
-    'ORDONANȚĂ pentru',
-    'Ordonanţă privind'
-  ],
-  OUG: [
-    'OUG',
-    'Ordonanţă de urgenţă a Guvernului',
-    'Ordonanta de urgenta privind',
-    'ORDONANŢĂ DE URGENȚĂ pentru',
-    'ORDONANŢĂ DE URGENȚĂ privind'
-  ],
-  PROIECT: [
-    'Schema de ajutor de minimis',
-    'proiect Hotărâre a Guvernului',
-    'proiect de Hotărâre a Guvernului',
-    'Proiect Ordonanţă de urgenţă',
-    /dezbaterea proiectului de Hotarare a Guvernului/,
-    'LEGE ratificarea'
-  ]
+    HG: [
+        'Hotărâre a Guvernului',
+        'Hotarare a Guvernului',
+        'HG',
+        'Hotărâre pentru',
+        'Hotarare privind',
+        'HOTĂRÂRE privind',
+        'HOTĂRÂRE pentru',
+        'HOTARARE pentru',
+        'Hotărâre de Guvern'
+    ],
+    OG: [
+        'Ordin al',
+        'ORDONANȚĂ pentru',
+        'Ordonanţă privind'
+    ],
+    OUG: [
+        'OUG',
+        'Ordonanţă de urgenţă a Guvernului',
+        'Ordonanta de urgenta privind',
+        'ORDONANŢĂ DE URGENȚĂ pentru',
+        'ORDONANŢĂ DE URGENȚĂ privind'
+    ],
+    PROIECT: [
+        'Schema de ajutor de minimis',
+        'proiect Hotărâre a Guvernului',
+        'proiect de Hotărâre a Guvernului',
+        'Proiect Ordonanţă de urgenţă',
+        /dezbaterea proiectului de Hotarare a Guvernului/,
+        'LEGE ratificarea'
+    ]
 };
 function getType($, title) {
-  "use strict";
+    let type;
 
-  let type;
+    Object.keys(regexClassificators)
+        .forEach(function (key) {
+            regexClassificators[key]
+                .forEach(function (pattern) {
+                    let result;
 
-  Object.keys(regexClassificators)
-    .forEach(function(key) {
-      regexClassificators[key]
-        .forEach(function(pattern) {
-          let result;
+                    if (type) {
+                        return;
+                    }
 
-          if (type) {
-            return;
-          }
+                    if (typeof pattern === 'string') {
+                        result = title
+                                .toLowerCase()
+                                .indexOf(pattern.toLowerCase()) === 0
+                    } else if (typeof pattern === 'object') {
+                        result = pattern.test(title);
+                    }
 
-          if (typeof pattern === 'string') {
-            result = title.toLowerCase().indexOf(pattern.toLowerCase()) === 0
-          } else if (typeof pattern === 'object') {
-            result = pattern.test(title);
-          }
-
-          if (result) {
-            type = key;
-          }
+                    if (result) {
+                        type = key;
+                    }
+                });
         });
-    });
 
-  return type
-    ? type
-    : '?';
+    return type
+        ? type
+        : null;
 }
+
+
+/** ====== month ====== */
 
 let months = {
-  'ianuarie':'01',
-  'februarie':'02',
-  'martie':'03',
-  'aprilie':'04',
-  'mai':'05',
-  'iunie':'06',
-  'iulie':'07',
-  'august':'08',
-  'septembrie':'09',
-  'octombrie':'10',
-  'noiembrie':'11',
-  'decembrie':'12'
+    'ianuarie': '01',
+    'februarie': '02',
+    'martie': '03',
+    'aprilie': '04',
+    'mai': '05',
+    'iunie': '06',
+    'iulie': '07',
+    'august': '08',
+    'septembrie': '09',
+    'octombrie': '10',
+    'noiembrie': '11',
+    'decembrie': '12'
 };
 function getDate($) {
-  let digitsArr = $('.item.column-1>b')
-    .eq(1)
-    .text()
-    .split(/\s/);
+    let digitsArr = $('.item.column-1>b')
+        .eq(1)
+        .text()
+        .split(/\s/);
 
-  digitsArr[1] = months[digitsArr[1].toLowerCase()];
+    digitsArr[1] = months[digitsArr[1]
+        .toLowerCase()];
+    // var scrappedDate = encodeURIComponent($(bolded[1]).text()).split('%C2%A0');
+    // modelProject.date = scrappedDate[2] +'-'+ allMonths[scrappedDate[1]] +'-'+ scrappedDate[0];
 
-  // var scrappedDate = encodeURIComponent($(bolded[1]).text()).split('%C2%A0');
-  // modelProject.date = scrappedDate[2] +'-'+ allMonths[scrappedDate[1]] +'-'+ scrappedDate[0];
-
-
-  return digitsArr.reverse().join('-');
+    return digitsArr
+        .reverse()
+        .join('-');
 }
+
+
+/** ====== feedback days no. ====== */
 
 function getFeedbackDays($, $listItem) {
-  "use strict";
+    let feedbackDaysPatternMatchingResult = $listItem
+        .text()
+        .match(/se primesc in termen de (\d{1,3}) zile de la data/i);
 
-  return parseInt($listItem
-    .text()
-    .match(/se primesc in termen de (\d{1,3}) zile de la data/i)[1]);
+    let daysString = feedbackDaysPatternMatchingResult
+                    && feedbackDaysPatternMatchingResult[1];
+
+    if (!daysString) {
+        return null; //TODO log helpful warn
+    }
+
+    return parseInt(daysString) || null;
 }
+
+
+/** ====== email ====== */
 
 function getEmail($, $listItem) {
-  "use strict";
-
-  return $listItem
-    .find('span')
-    .text();
+    return $listItem
+        .find('span')
+        .text();
 }
+
+
+/** ====== documents ====== */
 
 function getDocuments($, URL) {
-  "use strict";
+    let documents = $('li').find('a'),
+        parsedDocs = [];
 
-  let documents = $('li').find('a');
-  let parsedDocs = [];
-  documents.each(function(i, document) {
-    let doc = $(document).attr('href');
+    documents.each(function (i, document) {
+        let $doc = $(document);
 
-    if(doc.length) {
-        parsedDocs.push({
-            "type": $(document).text(),
-            "url": URL + doc
-        });
-    }
-  });
+        if ($doc.text()) {
+            parsedDocs.push({
+                type: $doc.text(),
+                url: URL + $doc.attr('href')
+            });
+        }
+    });
 
-  return parsedDocs;
+    return parsedDocs;
 }
+
+
+module.exports = parseProject;
