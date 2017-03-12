cdb38838c102bc8ceb13e8f4750ce8e0f00fd979
Ionut-Maxim Margelatu <imargela@adobe.com>
2017-03-12 18:59:19 +0200

    Completed the scraper for Cultura
    * improved parsing of the proposal type and removed half the code in the process
    * so success, much wow

--- cultura/src/main/java/ro/code4/czl/scrape/spider/cultura/CulturaPageProcessor.java
+++ cultura/src/main/java/ro/code4/czl/scrape/spider/cultura/CulturaPageProcessor.java
@@ -22,14 +22,12 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
  * @author Ionut-Maxim Margelatu (ionut.margelatu@gmail.com)
@@ -39,7 +37,7 @@ public class CulturaPageProcessor implements PageProcessor {
   private static final Logger logger = LoggerFactory.getLogger(CulturaPageProcessor.class);
 
   private static final Pattern PATTERN_PROJECT_TYPE = Pattern
-      .compile("(?:proiect de|proiectul de|proiect|proiectul) (?<projectType>\\w+)", Pattern.CASE_INSENSITIVE);
+      .compile("(?<projectType>hotarare|lege|oug|hg|ordin|og|om)", Pattern.CASE_INSENSITIVE);
 
   private static final Pattern PATTERN_FEEDBACK_DEADLINE = Pattern
       .compile("(?:pana la data de|pana la data) (?<feedbackDeadlineDate>\\d{1,2}\\.\\d{2}\\.\\d{4})", Pattern.CASE_INSENSITIVE);
@@ -88,81 +86,57 @@ public class CulturaPageProcessor implements PageProcessor {
       Map<String, String> contact = new HashMap<>();
       List<DocumentRepresentation> documents = new ArrayList<>();
 
-      if (proposalPageTitle.contains("|")) {
-        // single proposal on the page
+      title = proposalPageTitle;
+      identifier = DigestUtils.md5Hex(title);
 
-        logger.info("pageTitle={}", proposalPageTitle);
-
-        title = proposalPageTitle;
-        identifier = DigestUtils.md5Hex(title);
-
-        List<String> proposalPageTitleParts = Arrays.stream(proposalPageTitle.split("\\|")).map(String::trim).collect(Collectors.toList());
-        if (proposalPageTitleParts.size() == 2) {
-          String proposalTitleType = proposalPageTitleParts.get(0);
-          String proposalTitle = StringUtils.stripAccents(proposalPageTitleParts.get(1));
-          if (proposalTitleType.toLowerCase().startsWith("dezbatere")) {
-            Matcher projectTypeMatcher = PATTERN_PROJECT_TYPE.matcher(proposalTitle);
-            String projectTypeLabel = projectTypeMatcher.find() ? projectTypeMatcher.group("projectType") : null;
-            if (projectTypeLabel != null) {
-              type = ProposalType.fromLabel(projectTypeLabel);
-              logger.info("projectType={} projectTypeLabel={}", type, projectTypeLabel);
-            }
-          } else if (proposalTitleType.toLowerCase().startsWith("proiect")) {
-            Matcher projectTypeMatcher = PATTERN_PROJECT_TYPE.matcher(proposalTitleType);
-            String projectTypeLabel = projectTypeMatcher.find() ? projectTypeMatcher.group("projectType") : null;
-            if (projectTypeLabel != null) {
-              type = ProposalType.fromLabel(projectTypeLabel);
-              logger.info("projectType={} projectTypeLabel={}", type, projectTypeLabel);
-            }
-          } else {
-            logger.warn("Don't know how to parse {}", proposalPageTitle);
-          }
-        } else if (proposalPageTitleParts.size() >= 3) {
-
-        }
-
-        String extractedPageContent = page.getHtml().xpath("//*[@id='block-continutprincipalpagina']/div/article/div/div[2]/div[4]/div").toString();
-        description = Jsoup.parse(extractedPageContent
-                                      .replaceAll("<a([^>]+)>(.+?)</a>", "")
-                                      .replaceAll("Fisiere[^:]*:", "<br/>")
-                                      .replaceAll("Anexe:", "<br/>"))
-            .text();
-
-        page.getHtml().xpath("//*[@id='block-continutprincipalpagina']/div/article/div/div[2]/div[4]/div/ul/li/a").all().forEach(htmlLink -> {
-          Element documentLink = Jsoup.parse(htmlLink).select("a").first();
-          documents.add(new DocumentRepresentation(StringUtils.capitalize(documentLink.text().replaceFirst("^[^a-zA-Z]+", "").trim()),
-                                                   documentLink.attr("href")));
-        });
-
-        String strippedContent = StringUtils.stripAccents(description);
-        Matcher feedbackDeadlineMatcher = PATTERN_FEEDBACK_DEADLINE.matcher(strippedContent);
-        String extractedFeedbackDeadlineDate = feedbackDeadlineMatcher.find() ? feedbackDeadlineMatcher.group("feedbackDeadlineDate") : null;
-        if (extractedFeedbackDeadlineDate != null) {
-          Date feedbackDeadlineDate;
-          try {
-            feedbackDeadlineDate = siteFeedbackDateFormat.parse(extractedFeedbackDeadlineDate.replaceAll("\\.", " "));
-          } catch (ParseException e) {
-            feedbackDeadlineDate = null;
-          }
-          if (feedbackDeadlineDate != null) {
-            LocalDate now = LocalDate.now();
-            LocalDate then = feedbackDeadlineDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-
-            if (now.isBefore(then)) {
-              feedbackDays = ChronoUnit.DAYS.between(then, now);
-            }
-          }
-        }
-
-        Matcher contactEmailMatcher = PATTERN_CONTACT_EMAIL.matcher(strippedContent);
-        String contactEmail = contactEmailMatcher.find() ? contactEmailMatcher.group("email") : null;
-        if (contactEmail != null) {
-          contact.put("email", contactEmail);
-        }
-
+      String cleanedTitle = StringUtils.stripAccents(proposalPageTitle);
+      Matcher projectTypeMatcher = PATTERN_PROJECT_TYPE.matcher(cleanedTitle);
+      String projectTypeLabel = projectTypeMatcher.find() ? projectTypeMatcher.group("projectType") : null;
+      if (projectTypeLabel != null) {
+        type = ProposalType.fromLabel(projectTypeLabel);
+        logger.info("Parsed projectType={} projectTypeLabel={}", type, projectTypeLabel);
       } else {
-        // multiple proposals on the page (sigh..)
-        logger.warn("Found a page with multiple proposals url={}", page.getUrl());
+        logger.warn("Don't know how to parse title={} url={}", proposalPageTitle, page.getUrl());
+        return;
+      }
+
+      String extractedPageContent = page.getHtml().xpath("//*[@id='block-continutprincipalpagina']/div/article/div/div[2]/div[4]/div").toString();
+      description = Jsoup.parse(extractedPageContent
+                                    .replaceAll("<a([^>]+)>(.+?)</a>", "")
+                                    .replaceAll("Fisiere[^:]*:", "<br/>")
+                                    .replaceAll("Anexe:", "<br/>"))
+          .text();
+
+      page.getHtml().xpath("//*[@id='block-continutprincipalpagina']/div/article/div/div[2]/div[4]/div/ul/li/a").all().forEach(htmlLink -> {
+        Element documentLink = Jsoup.parse(htmlLink).select("a").first();
+        documents.add(new DocumentRepresentation(StringUtils.capitalize(documentLink.text().replaceFirst("^[^a-zA-Z]+", "").trim()),
+                                                 documentLink.attr("href")));
+      });
+
+      String strippedContent = StringUtils.stripAccents(description);
+      Matcher feedbackDeadlineMatcher = PATTERN_FEEDBACK_DEADLINE.matcher(strippedContent);
+      String extractedFeedbackDeadlineDate = feedbackDeadlineMatcher.find() ? feedbackDeadlineMatcher.group("feedbackDeadlineDate") : null;
+      if (extractedFeedbackDeadlineDate != null) {
+        Date feedbackDeadlineDate;
+        try {
+          feedbackDeadlineDate = siteFeedbackDateFormat.parse(extractedFeedbackDeadlineDate.replaceAll("\\.", " "));
+        } catch (ParseException e) {
+          feedbackDeadlineDate = null;
+        }
+        if (feedbackDeadlineDate != null) {
+          LocalDate now = LocalDate.now();
+          LocalDate then = feedbackDeadlineDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+          if (now.isBefore(then)) {
+            feedbackDays = ChronoUnit.DAYS.between(then, now);
+          }
+        }
+      }
+
+      Matcher contactEmailMatcher = PATTERN_CONTACT_EMAIL.matcher(strippedContent);
+      String contactEmail = contactEmailMatcher.find() ? contactEmailMatcher.group("email") : null;
+      if (contactEmail != null) {
+        contact.put("email", contactEmail);
       }
 
       String extractedDate = page.getHtml()
@@ -179,19 +153,17 @@ public class CulturaPageProcessor implements PageProcessor {
       }
 
       if (ObjectUtils.allNotNull(identifier, description, title, type, institution, feedbackDays, date)) {
-        // page.setSkip(false);
+        page.setSkip(false);
 
         page.putField("identifier", identifier);
         page.putField("description", description);
         page.putField("title", title);
-        page.putField("type", type);
+        page.putField("type", type.name());
         page.putField("institution", institution);
         page.putField("feedbackDays", feedbackDays);
         page.putField("date", date);
         page.putField("documents", documents);
         page.putField("contact", contact);
-
-        logger.info(page.getResultItems().toString());
       }
     }
   }
