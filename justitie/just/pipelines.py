4eb8e87e03d7f78f8b94084db32d47ebbcdb57be
Vlad Fratila <vlad.fratila@gmail.com>
2017-03-12 15:05:05 +0200

    log in the general scrapy log

--- justitie/just/pipelines.py
+++ justitie/just/pipelines.py
@@ -23,11 +23,11 @@ class JustPublicationsToApiPipeline(object):
 
         r = requests.post(API_PUBLICATIONS, json=dict(item), headers={'Authorization': 'Token %s' % (API_KEY,) } )
 
-        api_log = logging.getLogger('api-log.txt')
+
         if r.status_code == 200 or r.status_code == '200':
-            api_log.log(r.status_code, level=logging.INFO)
+            logging.log(msg=r.status_code, level=logging.INFO)
         else:
-            api_log.log(r.status_code, level=logging.ERROR)
-        api_log.log(r.content, level=logging.INFO)
+            logging.log(msg=r.status_code, level=logging.ERROR)
+            logging.log(msg=r.content, level=logging.INFO)
 
         return item
