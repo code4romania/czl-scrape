47eaae7cd8cd21673ed301c491b8230f27d2bff6
Ramona06 <ramona.cocias@gmail.com>
2017-03-12 11:17:38 +0000

    Added feedback_days

--- sanatate/scrapy_proj/spiders/sanatate.py
+++ sanatate/scrapy_proj/spiders/sanatate.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import scrapy_proj.items
+import re
 
 class SanatateSpider(scrapy.Spider):
     name = "sanatate"
@@ -17,6 +18,7 @@ class SanatateSpider(scrapy.Spider):
         date_regex = re.compile('\d{1,2}[-/]\d{2}[-/]\d{4}')
         email_regex = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
         tel_regex = re.compile(r'(0?([0-9].?){9})')
+        deadline_regex = re.compile(r'termen.*limita.*[^[0-9]]*([0-9]{1,2}).*zi')
 
         for item in response.css('.panel'):
             heading = item.css('div.panel-heading')
@@ -37,8 +39,8 @@ class SanatateSpider(scrapy.Spider):
             title = item.css('a.panel-title::text').extract_first()
             contact_tel = tel_regex.search(body_text).group(0)
             contact_email = email_regex.search(body_text).group(0)
-            item_dates = date_regex.findall(body_text)
-            start_date = item_dates[0]
+            start_date = date_regex.search(body_text).group(0)
+            feedback_days = deadline_regex.search(body_text.lower()).group(1)
 
             contact = {
                 'name': body.xpath('.//p[contains(text(), "Contact")]/text()').re_first(r'Contact:\s*(.*)'),
@@ -55,7 +57,8 @@ class SanatateSpider(scrapy.Spider):
                 title = title,
                 contact = contact,
                 documents = documents,
-                date = start_date
+                date = start_date,
+                feedback_days = feedback_days
             )
 
         next_pages = response.css('.pt-cv-pagination a::attr(href)').extract()
