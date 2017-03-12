e9da8068f1d7cd4e49e2a759404787b25ce3071a
Vlad Fratila <vlad.fratila@gmail.com>
2017-03-12 12:55:38 +0200

    add pipeline

--- justitie/just/settings.py
+++ justitie/just/settings.py
@@ -65,9 +65,9 @@ LOG_ENABLED = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#     'just.pipelines.JustPublicationsToApiPipeline': 100,
-# }
+ITEM_PIPELINES = {
+    'just.pipelines.JustPublicationsToApiPipeline': 100,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
