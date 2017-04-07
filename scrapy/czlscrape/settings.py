ad0c01a819af280d8020e7504b5440228abcdc1c
Alex Morega <alex@grep.ro>
2017-04-07 11:49:07 +0300

    set log level to DEBUG; log function name

--- scrapy/czlscrape/settings.py
+++ scrapy/czlscrape/settings.py
@@ -90,6 +90,6 @@ ITEM_PIPELINES = {
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
-LOG_LEVEL = 'WARN'
-LOG_FORMAT = '%(asctime)s srv="czl-scrape" [%(thread)d] %(levelname)s %(name)s: %(message)s'
+LOG_LEVEL = 'DEBUG'
+LOG_FORMAT = '%(asctime)s srv="czl-scrape" [%(thread)d] %(levelname)s %(name)s %(funcName)s: %(message)s'
 LOG_DATEFORMAT = '%Y-%m-%dT%H:%M:%SZ'
